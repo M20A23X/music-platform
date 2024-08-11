9ae886d5b995c488a0bcc3a503610fa0fe66caf1
Maksym Shapovalov <maksym.shapovalovv@gmail.com>
2024-08-11 19:10:53 -0400

    Initialize server package

---
+++ server/src/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common';
+import { TrackModule } from './track/track.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { FileModule } from './file/file.module';
+import * as path from 'path';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({ rootPath: path.resolve(__dirname, 'static') }),
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:123@cloud-storage.jr11v.mongodb.net/?retryWrites=true&w=majority&appName=cloud-storage',
+    ),
+    TrackModule,
+    FileModule,
+  ],
+})
+export class AppModule {}
